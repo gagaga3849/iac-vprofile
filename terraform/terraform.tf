67630f3aee10a58d7657048ecbe0156a6e576909
gagaga3849 <gagaga3849@gmail.com>
2025-02-08 16:23:06 +0100

    variables_changed

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,13 +27,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gigigarugamba"
+    bucket = "gitopsina"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = ">= 1.0.0, < 1.9.0"
 }
-##
-##
-##
+
